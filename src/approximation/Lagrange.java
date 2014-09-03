144c2a5dbe444fcfdf531fce59d00f9d9f1b8f11
Pavel Verkhovtsov <pavel_verkhovtsov@epam.com>
2014-09-03 20:02:31 +0300

    Small refactoring

--- src/approximation/Lagrange.java
+++ src/approximation/Lagrange.java
@@ -1,20 +1,33 @@
 package approximation;
 
+/**
+ * Class contained methods to approximate function by Lagrange.
+ * @author Pavel_Verkhovtsov
+ */
 public class Lagrange {
 	private double[] arrayX;
 	private double[] arrayY;
-	private double point;
-	private double l = 0;
-	
 
 
-	public Lagrange(double[] xArray, double[] yArray, double p){ // TODO Normal names of variable (p,l,point,s)
+
+	/**
+	 * Constructor
+	 * @param xArray Array of x coordinate
+	 * @param yArray Array of y coordinate
+	 */
+	public Lagrange(final double[] xArray, final double[] yArray){
+		// TODO Normal names of variable (p,point,s)
 		arrayX=xArray;
 		arrayY=yArray;
-		point = p;
 	}
-	
-	public double approximazeFunction(){
+
+	/**
+	 * Approximate function by Lagrange.
+	 * @param point point of approximated
+	 * @return value approximated function in point
+	 */
+	public double approximazeFunction(final double point){
+		double valueInPoint = 0;
 		for(int i=0; i<arrayX.length; i++){
 			double s=1;
 			for(int j=0; j<arrayY.length; j++){
@@ -22,8 +35,8 @@ public class Lagrange {
 					s*=(point-arrayX[j])/(arrayX[i]-arrayX[j]);
 				}
 			}
-			l+=arrayY[i]*s;
+			valueInPoint+=arrayY[i]*s;
 		}
-		return l;
+		return valueInPoint;
 	}
-}	
+}
--- src/main/Main.java
+++ src/main/Main.java
@@ -2,13 +2,21 @@ package main;
 
 import approximation.Lagrange;
 
+/**
+ * Main class
+ * @author Pavel_Verkhovtsov
+ */
 public class Main {
 
 	private static double[] y = {2.02, 1.98, 1.67, 1.65, 1.57, 1.42, 1.37, 1.07, 0.85, 0.48, 0.35, -0.30, -0.61, -1.2, -1.39, -1.76, -2.28, -2.81, -3.57, -4.06};
 	private static double[] x = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
 
+	/**
+	 * Main method.
+	 * @param args console parameters
+	 */
 	public static void main(final String[] args){
-		Lagrange lagrange = new Lagrange(x, y, 2);
-		System.out.println(lagrange.approximazeFunction());
+		Lagrange lagrange = new Lagrange(x, y);
+		System.out.println(lagrange.approximazeFunction(2));
 	}
 }
